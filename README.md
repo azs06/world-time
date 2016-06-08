76ba06521445383eddef66d8970ef00d2f61b09f
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2016-06-09 00:24:01 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# World-time
+
+A world clock using vuejs, moment and moment timezone.
+
+## Prerequisites
+
+You will need the following things properly installed on your computer.
+
+* [Git](http://git-scm.com/)
+* [Node.js](http://nodejs.org/) (with NPM)
+* [Bower](http://bower.io/)
+* [VueJs](http://vuejs.org/)
+
+## Installation
+
+* `git clone <repository-url>` this repository
+* change into the new directory
+* `bower install`
+
---
+++ js/app.js
@@ -0,0 +1,61 @@
+var vue = new Vue({
+	el: "#app",
+	ready: function(){
+		var _this = this;
+		setInterval(function(){
+			_this.counter += 1;
+			_this.updateTime();
+		}, 1000)
+	},
+	data:{
+		message: "Hello from vue",
+		counter: 0,
+		newTodo: "",
+		todos: [
+		{text: "Todo item 1"},
+		{text: "Todo item 2"},
+		{text: "Todo item 2"}
+		],
+		selectedTimeZone: null,
+		times: []
+	},
+	methods: {
+		reverseMessage: function(){
+			this.message = this.message.split("").reverse().join("");
+		},
+		getZoneTime: function(){
+			var zoneTime = moment().tz(this.selectedTimeZone).format('h:mm:ss a');
+			this.times.push({
+				name: this.selectedTimeZone,
+				time: zoneTime
+			});
+			console.log(zoneTime);
+		},
+		updateTime: function(){
+			if(this.times.length > 0){
+				for(var i=0; i< this.times.length; i++){
+					this.times[i].time = moment().tz(this.times[i].name).format('h:mm:ss a');
+				}
+			}
+		}
+	},
+	computed:{
+		time: function(){
+			this.counter;
+			return moment().format('h:mm:ss a');		
+		},
+		timezones: function(){
+			var timezones = [];
+			var timezone = {};
+			for (var i in moment.tz._zones) {
+				timezone = moment.tz.unpack(moment.tz._zones[i]);
+				timezones.push({
+					name: timezone.name,
+					offset: timezone.offsets[0]	
+				});      		
+    		}
+    		return timezones;
+		}
+	}
+});
+
